af029871597cae8cb1c0f1e79d8134b546c55b80
Khang Hoang <hoangtrieukhang@gmail.com>
2017-08-25 13:30:11 +0700

    wip/apply thunk actions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 import React, { Component } from 'react';
+import { connect, Provider} from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logo from './logo.svg';
 import './App.css';
+
+const userReducer = (state = { users: [], isFetching: false }, action) => {
+  switch (action.type) {
+    case 'FETCH_START':
+      return {
+        isFetching: true,
+        users: [],
+      }
+    case 'FETCH_END':
+      return {
+        isFetching: true,
+        users: action.payload,
+      }
+    default: {
+      return state;
+    }
+  }
+}
+
+const thunkMiddleware = store => next => action => {
+  if (typeof action === 'function') {
+    action(store);
+    return;
+  }
+
+  next(action);
+};
+
+const fetchUserAction = ({ dispatch }) => {
+  dispatch({ type: 'FETCH_START' });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      return res.json();
+    })
+    .then(users => {
+      dispatch({ type: 'FETCH_END', users, });
+    });
+}
+
+const store = createStore(() => ({}), {}, applyMiddleware([thunkMiddleware]));
 
 const Username = ({ username }) => {
   return (
@@ -19,29 +61,22 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => {
-        return res.json();
-      })
-      .then(users => {
-        this.setState({
-          users,
-        });
-      });
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Provider store={store}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <div className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            {this.state.users.map(u => <Username username={u.name} />)}
+          </div>
         </div>
-        <div className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          {this.state.users.map(u => <Username username={u.name} />)}
-        </div>
-      </div>
+      </Provider>
     );
   }
 }
